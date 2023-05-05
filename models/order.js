d5b750ff6deeabce91e1fa8ba3ddd8db70b48d32
Felicia Putri <fwanggai245@gmail.com>
2023-05-05 12:31:30 +0700

    update order.js for models

--- models/order.js
+++ models/order.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Order extends Model {
     /**
@@ -10,26 +8,40 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.belongsTo(models.Product, {foreignKey: 'codeProduct'});
-      this.belongsTo(models.Customer, {foreignKey: 'id'});
-      this.hasOne(models.Payment, {foreignKey: 'codePayment'});
-      this.hasOne(models.Review, {foreignKey: 'codeOrder'});
+      this.belongsTo(models.Product, { foreignKey: "codeProduct" });
+      this.belongsTo(models.Customer, { foreignKey: "id" });
+      this.hasOne(models.Payment, { foreignKey: "codePayment" });
+      this.hasOne(models.Review, { foreignKey: "codeOrder" });
     }
   }
-  Order.init({
-    codeProduct: DataTypes.STRING,
-    quantity: DataTypes.INTEGER,
-    customerId:  DataTypes.INTEGER,
-    courierJne: DataTypes.ENUM(['OKE','REG','SPS','YES']),
-    origin: DataTypes.STRING,
-    destination: DataTypes.STRING,
-    weight: DataTypes.INTEGER,
-    costCourier: DataTypes.INTEGER,
-    statusOrder: DataTypes.ENUM(['PENDING', 'PROCESS', 'DELIVERED', 'CANCEL'])
-  }, {
-    sequelize,
-    modelName: 'Order',
-    tableName: "orders"
-  });
+  Order.init(
+    {
+      codeOrder: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+      },
+      codeProduct: {
+        type: DataTypes.STRING
+      },
+      quantity: DataTypes.INTEGER,
+      customerId: DataTypes.INTEGER,
+      courierJne: DataTypes.ENUM(["OKE", "REG", "SPS", "YES"]),
+      origin: DataTypes.STRING,
+      destination: DataTypes.STRING,
+      weight: DataTypes.INTEGER,
+      costCourier: DataTypes.INTEGER,
+      statusOrder: DataTypes.ENUM([
+        "PENDING",
+        "PROCESS",
+        "DELIVERED",
+        "CANCEL",
+      ]),
+    },
+    {
+      sequelize,
+      modelName: "Order",
+      tableName: "orders",
+    }
+  );
   return Order;
 };
