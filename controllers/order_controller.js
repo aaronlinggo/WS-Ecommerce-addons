982369da0a58777e61e58e775484b7884099961c
leleleleilel <markeuuu8@gmail.com>
2023-05-05 20:23:02 +0700

    Revisi Order (model & associate)

--- controllers/order_controller.js
+++ controllers/order_controller.js
@@ -1,10 +1,11 @@
-const { sequelize } = require('../models');
+const {
+  sequelize
+} = require('../models');
 
 const Order = require('../models').Order;
 
 const getAllOrder = async (req, res) => {
   let orders = await Order.findAll();
-
   return res.status(200).send(orders);
 }
 
