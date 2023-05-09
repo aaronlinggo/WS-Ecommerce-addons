d004ea89c82771ce14901a78dd306de28088f447
VindSend <vincenttj59@gmail.com>
2023-05-09 11:36:40 +0700

    Add to cart sudah selesai (sampai validation)

--- routes/CustomerRoutes.js
+++ routes/CustomerRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { Op } = require("sequelize");
 
 const CCustomer = require("../controllers/CustomerController");
-const COrder = require("../controllers/OrderController");
+// const COrder = require("../controllers/OrderController");
 
 const Order = require('../models').Order;
 const Customer = require('../models').Customer;
@@ -13,73 +13,103 @@ const { check } = require("express-validator");
 const router = express.Router();
 
 // /customer
-//GET
+//VIEW ORDER
+//params =  customerId (1-20)
+//body   =  codeOrder (optional
+
 router.get("/order/viewOrder/:customerId",
-check("customerId").custom((value) => {
-    return Customer.findOne({ where: { id: value } }).then((user) => {
-        if (!user) {
-            return Promise.reject("Customer dengan Id tersebut tidak ditemukan");
-        }
+    check("customerId").custom((value) => {
+        return Customer.findOne({ where: { id: value } }).then((user) => {
+            if (!user) {
+                return Promise.reject("Customer dengan Id tersebut tidak ditemukan");
+            }
+        })
     })
-})
-,COrder.viewOrder);
+    , CCustomer.viewOrder);
 
+
+//PAY ORDER
+//params =  customerId (1-20)
+//body   =  codeOrder
 router.post("/order/payOrder/:customerId",
-check("customerId").custom((value) => {
-    return Customer.findOne({ where: { id: value } }).then((user) => {
-        if (!user) {
-            return Promise.reject("Customer dengan Id tersebut tidak ditemukan");
-        }
+    check("customerId").custom((value) => {
+        return Customer.findOne({ where: { id: value } }).then((user) => {
+            if (!user) {
+                return Promise.reject("Customer dengan Id tersebut tidak ditemukan");
+            }
+        })
+    }),
+    check("codeOrder").custom((value) => {
+        return Order.findOne({ where: { codeOrder: value } }).then((order) => {
+            if (!order) {
+                return Promise.reject("Order dengan Id tersebut tidak ditemukan");
+            }
+        })
     })
-}),
-check("codeOrder").custom((value) => {
-    return Order.findOne({ where: { codeOrder : value } }).then((order) => {
-        if (!order) {
-            return Promise.reject("Order dengan Id tersebut tidak ditemukan");
-        }
-    })
-})
-,COrder.payOrder);
+    , CCustomer.payOrder);
 
+//Check Out
+//params =  customerId (1-20)
+//body =    courierJne (OKE/REG/SPS/YES), 
+//          origin (1-500), 
+//          destination (1-500)
 
 router.post("/order/checkout/:customerId",
-check("customerId").custom((value) => {
-    return Customer.findOne({ where: { id: value } }).then((user) => {
-        if (!user) {
-            return Promise.reject("Customer dengan Id tersebut tidak ditemukan");
+    check("customerId").custom((value) => {
+        return Customer.findOne({ where: { id: value } }).then((user) => {
+            if (!user) {
+                return Promise.reject("Customer dengan Id tersebut tidak ditemukan");
+            }
+        })
+    }),
+    check("courierJne").custom((value) => {
+        if (value == "OKE" || value == "REG" || value == "SPS" || value == "YES") {
+            return true;
         }
-    })
-}),
-check("courierJne").custom((value) => {
-    if(value=="OKE" || value=="REG" || value=="SPS" || value=="YES"){
+        else {
+            return Promise.reject("Layanan yang tersedia hanya OKE,REG,SPS,YES");
+        }
+    }),
+    check("origin").custom((value) => {
+        if (value < 1 || value > 500) {
+            return Promise.reject("Kode kota asal hanya boleh dari 1-500");
+        }
         return true;
-    }
-    else{
-        return Promise.reject("Layanan yang tersedia hanya OKE,REG,SPS,YES");
-    }
-}),
-check("origin").custom((value) => {
-    if(value<1 || value>500){
-        return Promise.reject("Kode kota asal hanya boleh dari 1-500");
-    }
-    return true;
-}),
-check("destination").custom((value) => {
-    if(value<1 || value>500){
-        return Promise.reject("Kode kota tujuan hanya boleh dari 1-500");
-    }
-    return true;
-})
-,COrder.checkOut);
-
-router.post("/addToCart/:customerId",
-check("customerId").custom((value) => {
-    return Customer.findOne({ where: { id: value } }).then((user) => {
-        if (!user) {
-            return Promise.reject("Customer dengan Id tersebut tidak ditemukan");
+    }),
+    check("destination").custom((value) => {
+        if (value < 1 || value > 500) {
+            return Promise.reject("Kode kota tujuan hanya boleh dari 1-500");
         }
+        return true;
     })
-})
-,COrder.addToCart);
+    , CCustomer.checkOut);
+
+//ADD TO CART
+//params =  customerId (1-20)
+//body =    quantity (1 atau 1,2,3,4,....), 
+//          codeProduct (WSEC00002 / WSEC00001,WSEC00003), 
+//          nameProduct (Small Fresh Car / Small Fresh Car,Electronic Steel Car)
+router.post("/addToCart/:customerId",
+    check("customerId").custom((value) => {
+        return Customer.findOne({ where: { id: value } }).then((user) => {
+            if (!user) {
+                return Promise.reject("Customer dengan Id tersebut tidak ditemukan");
+            }
+        })
+    }),
+    check("quantity").notEmpty().withMessage("Quantity harus diisi (1,2,...)!"),
+    check("quantity").custom((value) => {
+        let panjangQty = value.split(",");
+        for (let i = 0; i < panjangQty.length; i++) {
+            let number = +panjangQty[i];
+            if (number) {
+            } else {
+                return Promise.reject("Quantity hanya boleh berisi angka (1,2,...)!");
+            }
+            // return Promise.reject(panjangQty.length);
+        }
+        return true;
+    })
+    , CCustomer.addToCart);
 
 module.exports = router;
