538105f672eec758700c988a19854289116834b8
Felicia Putri <fwanggai245@gmail.com>
2023-05-12 13:05:50 +0700

    modified order_routes.js - review_routes.js

--- routes/order_routes.js
+++ routes/order_routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const order_controller = require('../controllers/order_controller');
 
-router.get('/seeAllRequestOrder', order_controller.seeAllRequestOrder);
+router.get('/getAllOrder/:code_order?', order_controller.getAllOrder);
+router.get('/getAllRequestOrder', order_controller.getAllRequestOrder);
 router.put('/acceptOrder/:id', order_controller.acceptOrder);
 router.put('/completeOrder/:id', order_controller.completeOrder);
 router.put('/cancelOrder/:id', order_controller.cancelOrder);
-router.get('/getAllOrder/', order_controller.getAllOrder);
 
 module.exports = router;
