3c1632ba5dad0c1b1920d1915c9516b9d83423cd
leleleleilel <markeuuu8@gmail.com>
2023-05-12 19:57:40 +0700

    Revisi model customer & developer

--- models/customer.js
+++ models/customer.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.belongsTo(models.Developer, {foreignKey: 'id'});
+      this.belongsTo(models.Developer, {foreignKey: 'developerId'});
       this.hasMany(models.Order, {foreignKey: 'customerId'});
       this.hasMany(models.Review, {foreignKey: 'customerId'});
       this.hasMany(models.Cart, {foreignKey : 'customerId'});
