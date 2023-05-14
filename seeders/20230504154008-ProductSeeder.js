0a1541e4295c2a0f3c230a80d6c8ea582febde57
aaronlinggo <aaronlinggosatria@gmail.com>
2023-05-14 12:31:47 +0700

    Move Folder productDummy to assets

--- seeders/20230504154008-ProductSeeder.js
+++ seeders/20230504154008-ProductSeeder.js
@@ -73,7 +73,7 @@ function generateFakerItems(rowCount) {
 
     let randomNumberPhoto = faker.helpers.arrayElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
     let codeProduct = "WSEC" + ((data.length + 1) + "").padStart(5, '0');
-    fs.copyFile(`./storage/productDummy/${(randomNumberPhoto + "").padStart(3, '0')}_smc_ecom.jpg`, `./storage/${USERNAME[developerId-1]}/${codeProduct}.jpg`, (err) => {
+    fs.copyFile(`./assets/productDummy/${(randomNumberPhoto + "").padStart(3, '0')}_smc_ecom.jpg`, `./storage/${USERNAME[developerId-1]}/${codeProduct}.jpg`, (err) => {
       if (err) throw err;
     });
     const newItem = {
