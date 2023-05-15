2d8f576171fb822f98620e5b53f039695c4917b1
Felicia Putri <fwanggai245@gmail.com>
2023-05-15 22:36:04 +0700

    deleted bulkInsert Payment

--- seeders/20230504154028-PaymentSeeder.js
+++ seeders/20230504154028-PaymentSeeder.js
@@ -3,24 +3,24 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Payments', [
-      {
-        codePayment: 'INVOICEORDER00001',
-        codeOrder: 'OR00001',
-        subtotal: 14000,
-        paymentStatus: 'unpaid',
-        createdAt: Date.now(),
-        updatedAt: Date.now()
-      },
-      {
-        codePayment: 'INVOICEORDER00002',
-        codeOrder: 'OR00002',
-        subtotal: 30000,
-        paymentStatus: 'paid',
-        createdAt: Date.now(),
-        updatedAt: Date.now()
-      }
-    ]);
+    // await queryInterface.bulkInsert('Payments', [
+    //   {
+    //     codePayment: 'INVOICEORDER00001',
+    //     codeOrder: 'OR00001',
+    //     subtotal: 14000,
+    //     paymentStatus: 'unpaid',
+    //     createdAt: Date.now(),
+    //     updatedAt: Date.now()
+    //   },
+    //   {
+    //     codePayment: 'INVOICEORDER00002',
+    //     codeOrder: 'OR00002',
+    //     subtotal: 30000,
+    //     paymentStatus: 'paid',
+    //     createdAt: Date.now(),
+    //     updatedAt: Date.now()
+    //   }
+    // ]);
   },
 
   async down (queryInterface, Sequelize) {
