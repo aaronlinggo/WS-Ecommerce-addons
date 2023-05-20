51aee03e7d1376c5bbf5f071d9dc6b0c485a5689
aaronlinggo <aaronlinggosatria@gmail.com>
2023-05-20 21:32:48 +0700

    Update 20230430095930-create-review.js

--- migrations/20230430095930-create-review.js
+++ migrations/20230430095930-create-review.js
@@ -17,7 +17,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: {
-          model: 'cCustomers',
+          model: 'Customers',
           key: 'id'
         },
       },
