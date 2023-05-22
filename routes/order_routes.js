8a4630c76f8e20ba32f5120a6dce002386035fae
aaronlinggo <aaronlinggosatria@gmail.com>
2023-05-22 08:37:04 +0700

    Add Middleware Subscription

--- routes/order_routes.js
+++ routes/order_routes.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const order_controller = require('../controllers/order_controller');
 const authMiddleware = require("../middleware/AuthMiddleware")
 
-router.get('/getAllOrder/:code_order?', authMiddleware.developerMiddleware, order_controller.getAllOrder);
-router.get('/getAllRequestOrder', authMiddleware.developerMiddleware, order_controller.getAllRequestOrder);
-router.put('/acceptOrder/:id', authMiddleware.developerMiddleware, order_controller.acceptOrder);
-router.put('/completeOrder/:id', authMiddleware.developerMiddleware, order_controller.completeOrder);
-router.put('/cancelOrder/:id', authMiddleware.developerMiddleware, order_controller.cancelOrder);
+router.get('/getAllOrder/:code_order?', authMiddleware.developerMiddleware, subscriptionMiddleware, order_controller.getAllOrder);
+router.get('/getAllRequestOrder', authMiddleware.developerMiddleware, subscriptionMiddleware, order_controller.getAllRequestOrder);
+router.put('/acceptOrder/:id', authMiddleware.developerMiddleware, subscriptionMiddleware, order_controller.acceptOrder);
+router.put('/completeOrder/:id', authMiddleware.developerMiddleware, subscriptionMiddleware, order_controller.completeOrder);
+router.put('/cancelOrder/:id', authMiddleware.developerMiddleware, subscriptionMiddleware, order_controller.cancelOrder);
 
 module.exports = router;
